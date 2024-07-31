af01751ef9d3443af64701f0e5a1f943e9cc4454
Wil Thomason <wil.thomason@gmail.com>
2024-07-30 19:09:30 -0500

    chore: Update CPM.cmake pin

--- cmake/FetchInitCPM.cmake
+++ cmake/FetchInitCPM.cmake
@@ -1,8 +1,8 @@
 file(
   DOWNLOAD
-  https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.38.7/CPM.cmake
+  https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.40.1/CPM.cmake
   ${CMAKE_CURRENT_BINARY_DIR}/cmake/CPM.cmake
-  EXPECTED_HASH SHA256=83e5eb71b2bbb8b1f2ad38f1950287a057624e385c238f6087f94cdfc44af9c5
+  EXPECTED_HASH SHA256=117cbf2711572f113bab262933eb5187b08cfc06dce0714a1ee94f2183ddc3ec
 )
 set(CPM_USE_LOCAL_PACKAGES ON)
 include(${CMAKE_CURRENT_BINARY_DIR}/cmake/CPM.cmake)

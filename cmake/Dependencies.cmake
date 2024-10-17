b4eed0393221fe6d25d8f4e930dad371f19bcce7
Wil Thomason <wil.thomason@gmail.com>
2024-10-17 10:37:48 -0400

    Try to create exported CMake targets for the Nigh and pdqsort dependencies

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,10 +1,40 @@
 find_package(Eigen3 REQUIRED NO_MODULE)
 
 CPMAddPackage("gh:kavrakilab/nigh#97130999440647c204e0265d05a997dbd8da4e70")
-set(NIGH_INCLUDE_DIRS ${nigh_SOURCE_DIR}/src)
+if(nigh_ADDED)
+  add_library(nigh INTERFACE)
+  target_include_directories(nigh INTERFACE $<BUILD_INTERFACE:${nigh_SOURCE_DIR}/src>)
+  install(TARGETS nigh
+        EXPORT nigh_TARGETS
+        LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+        INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+  )
+  install(DIRECTORY ${nigh_SOURCE_DIR}/src/ DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+  install(EXPORT nigh_TARGETS FILE nighTargets.cmake NAMESPACE nigh:: DESTINATION
+    ${CMAKE_INSTALL_LIBDIR}/cmake/nigh)
+else()
+  message(FATAL_ERROR "Could not install required dependency: nigh")
+endif()
 
 CPMAddPackage("gh:orlp/pdqsort#b1ef26a55cdb60d236a5cb199c4234c704f46726")
-set(PDQSORT_INCLUDE_DIRS ${pdqsort_SOURCE_DIR})
+if(pdqsort_ADDED)
+  add_library(pdqsort INTERFACE)
+  target_include_directories(pdqsort INTERFACE $<BUILD_INTERFACE:${pdqsort_SOURCE_DIR}>)
+  install(TARGETS pdqsort
+        EXPORT pdqsort_TARGETS
+        LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+        INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+  )
+  install(DIRECTORY ${pdqsort_SOURCE_DIR} DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+  install(EXPORT pdqsort_TARGETS FILE pdqsortTargets.cmake NAMESPACE pdqsort:: DESTINATION
+    ${CMAKE_INSTALL_LIBDIR}/cmake/pdqsort)
+else()
+  message(FATAL_ERROR "Could not install required dependency: pdqsort")
+endif()
 
 # TODO: Handle including this in the C++ library
 if(CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")

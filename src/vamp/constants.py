cca0e1a31e067e16f6ec5d8b565651b6498f81d3
Wil Thomason <wil.thomason@gmail.com>
2025-07-24 15:31:32 -0400

    Add Spot files

--- src/vamp/constants.py
+++ src/vamp/constants.py
@@ -1,11 +1,14 @@
 DEFAULT_ITERATIONS = 1000000
 
+# TODO: These need to be updated automatically for new robots - or ideally, we read these values
+# from the robot module directly
 ROBOT_RRT_RANGES = {
     "sphere": 1,
     "ur5": 1.5,
     "panda": 1.0,
     "fetch": 1.0,
     "baxter": 0.5,
+    "spot": 1.0,
     }
 
 ROBOT_FIRST_JOINT_LOCATIONS = {

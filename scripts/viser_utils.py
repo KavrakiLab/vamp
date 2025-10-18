43429d12905df63f959add00ba0b5b9adab4d653
ShrutheeshIR <shrutheesh99@gmail.com>
2025-10-18 17:07:47 -0400

    attachment viz works

--- scripts/viser_utils.py
+++ scripts/viser_utils.py
@@ -1,14 +1,12 @@
 from viser.extras import ViserUrdf
 import viser
 import yourdfpy 
-from pathlib import Path
-from typing import Sequence
+from typing import Sequence, Union
 
-def setup_viser_with_robot():
+def setup_viser_with_robot(robot_dir, robot_urdf_name):
     server = viser.ViserServer()
     # change the robot here
-    robot_dir = Path(__file__).parents[1] / 'resources' / 'panda'
-    urdf = yourdfpy.URDF.load(str(robot_dir / "panda_spherized.urdf"))
+    urdf = yourdfpy.URDF.load(str(robot_dir / robot_urdf_name))
     robot = ViserUrdf(  
         server,  
         urdf,
@@ -23,18 +21,26 @@ def add_spheres(
     server: viser.ViserServer,
     sphere_positions: Sequence,
     sphere_radii : Sequence,
-    prefix: str = 'my_sphere'
+    colors: Union[Sequence[int], Sequence[Sequence[int]]] = [],
+    prefix: str = 'my_sphere',
 ):
     """
     Add spheres to the env/
     Sphere positions are (N,3) and sphere radii are (N)
     """
     sphere_handles = [None] * len(sphere_positions)
+    if len(colors) == 0:
+        colors = [[255, 0, 0]] * len(sphere_positions)
+    elif len(colors) == 1:
+        colors = colors * len(sphere_positions)
+    else:
+        assert len(colors) == len(sphere_positions)
     for i, (sphere_pos, sphere_rad) in enumerate(zip(sphere_positions, sphere_radii)):
-        sphere_handles[i] = server.scene.add_icosphere(  
+        sphere_handles[i] = server.scene.add_icosphere(
             name=f"{prefix}_{i}",
             radius=sphere_rad,  
-            position=tuple(sphere_pos[:3]),  
+            position=tuple(sphere_pos[:3]),
+            color = tuple(colors[i])
         )
     return sphere_handles
 
@@ -49,14 +55,14 @@ def add_trajectory(server, waypoints, robot, attachment_handles, attachment_posi
         robot (ViserUrdf): ViserUrdf instance of the robot
 
         attachment_handles (numpy.array) - this is a P element list of attachment handles, spheres here
-        attachment_positions (numpy.array) - this is a (P, N, 3) array of the position of each attachment handle at each waypoint pos.
+        attachment_positions (numpy.array) - this is a (N, P, 3) array of the position of each attachment handle at each waypoint pos.
 
     Returns:
         return_type: None.
     """
     if len(waypoints) < 1:
         return
-    assert len(attachment_handles) == len(attachment_positions)
+    assert len(attachment_handles) == len(attachment_positions[0])
     traj_slider = server.gui.add_slider(  
         "Current Waypoint",   
         min=0,   
@@ -71,4 +77,4 @@ def add_trajectory(server, waypoints, robot, attachment_handles, attachment_posi
         robot.update_cfg(joint_config)
 
         for attach_idx, attachment_handle in enumerate(attachment_handles):
-            attachment_handle.position = attachment_positions[attach_idx][waypoint_idx]
+            attachment_handle.position = attachment_positions[waypoint_idx][attach_idx]
--- scripts/visualize_viser.py
+++ scripts/visualize_viser.py
@@ -1,36 +1,36 @@
 import numpy as np
 from viser import transforms as tf
-import yourdfpy 
 import os
 from viser_utils import setup_viser_with_robot, add_spheres, add_trajectory
+from pathlib import Path
 
 import vamp
 from fire import Fire
 
 
 # Starting configuration
-a = [0., -0.785, 0., -2.356, 0., 1.571, 0.785]
+a = [0., -0.785, 0., -2.356, 0., 1.571, 0.785, 2.35, 1., 0., -0.8, 0, 2.5, 0.785]
 
 # Goal configuration
-b = [2.35, 1., 0., -0.8, 0, 2.5, 0.785]
+b = [-2.35, 1., 0., -0.8, 0, 2.5, 0.785, 0., -0.785, 0., -2.356, 0., 1.571, 0.785]
 
 
 # Problem specification: a list of sphere centers
 problem = [
-    [0.55, 0, 0.25],
-    [0.35, 0.35, 0.25],
-    [0, 0.55, 0.25],
-    [-0.55, 0, 0.25],
-    [-0.35, -0.35, 0.25],
-    [0, -0.55, 0.25],
-    [0.35, -0.35, 0.25],
-    [0.35, 0.35, 0.8],
-    [0, 0.55, 0.8],
-    [-0.35, 0.35, 0.8],
+    [0.55, 0, 0.15],
+    [0.35, 0.45, 0.15],
+    [0, 0.65, 0.15],
+    [-0.55, 0, 0.15],
+    [-0.35, -0.55, 0.15],
+    [0, -0.65, 0.15],
+    [0.35, -0.55, 0.15],
+    [0.35, 0.6, 0.8],
+    [0, 0.6, 0.8],
+    [-0.35, 0.5, 0.8],
     [-0.55, 0, 0.8],
-    [-0.35, -0.35, 0.8],
-    [0, -0.55, 0.8],
-    [0.35, -0.35, 0.8],
+    [-0.35, -0.5, 0.8],
+    [0, -0.65, 0.8],
+    [0.35, -0.65, 0.8],
     ]
 
 
@@ -38,23 +38,29 @@ problem = [
 def main(
     obstacle_radius: float = 0.2,
     attachment_radius: float = 0.07,
-    attachment_offset: float = 0.02,
+    attachment_offset: float = 0.04,
     planner: str = "rrtc",
     **kwargs,
     ):
 
     (vamp_module, planner_func, plan_settings,
-     simp_settings) = vamp.configure_robot_and_planner_with_kwargs("panda", planner, **kwargs)
+     simp_settings) = vamp.configure_robot_and_planner_with_kwargs("bimanualpanda", planner, **kwargs)
 
     # Create an attachment offset on the Z-axis from the end-effector frame
     tf = np.identity(4)
     tf[:3, 3] = np.array([0, 0, attachment_offset])
     attachment = vamp.Attachment(tf)
 
+    attachment2 = vamp.Attachment(tf)
+
     # Add a single sphere to the attachment - spheres are added in the attachment's local frame
     attachment.add_spheres([vamp.Sphere([0, 0, 0], attachment_radius)])
+    attachment2.add_spheres([vamp.Sphere([0, 0, 0], attachment_radius)])
 
-    server, robot = setup_viser_with_robot()
+
+    robot_dir = Path(__file__).parents[1] / 'resources' / 'panda'
+
+    server, robot = setup_viser_with_robot(robot_dir, "bipanda_spherized.urdf")
     robot.update_cfg(a)
 
 
@@ -66,16 +72,18 @@ def main(
     _problem_sphere_handles = add_spheres(server, np.array(problem), np.array([obstacle_radius] * len(problem)))
 
     # Add the attchment to the VAMP environment
-    e.attach(attachment)
+    e.attach(attachment, 0)
+    e.attach(attachment2, 1)
 
     # Add attachment sphere to visualization
-    attachment_sph = add_spheres(server, np.array([[0.0, 0.0, 0.0]]), np.array([attachment_radius]))[0]
+    attachment_sph = add_spheres(server, np.zeros((2, 3)), np.array([attachment_radius] * 2), colors=[[0, 255, 0], [0, 0, 255]])
 
     # Update attachment sphere positions corresponding to the waypoints.
     # this could also be made into a callable that can be called during trajectory viz
     def get_attachment_pos(configuration):
-        attachment.set_ee_pose(vamp_module.eefk(configuration))
-        return np.array(attachment.posed_spheres[0].position)
+        for idx, attach in enumerate([attachment, attachment2]):
+            attach.set_ee_pose(vamp_module.eefk(configuration)[idx])
+        return np.array([attachment.posed_spheres[0].position, attachment2.posed_spheres[0].position])
 
 
     # Plan and display
@@ -86,7 +94,7 @@ def main(
 
     attachment_positions = [get_attachment_pos(pos) for pos in simple.path.numpy()]
 
-    add_trajectory(server, simple.path.numpy(), robot, [attachment_sph], [attachment_positions])
+    add_trajectory(server, simple.path.numpy(), robot, attachment_sph, attachment_positions)
 
     # display
     while True:
